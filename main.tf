fbfbafe96aaeb5aedd952fdd7b2d7b29f49a26e3
sankalp139 <sankalp139@gmail.com>
2024-10-13 08:35:38 -0500

    added a resource usig import

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "EC2Test" {
     #instance configuration
+    ami="ami-0fff1b9a61dec8a5f"
+    instance_type="t2.micro"
+    tags= {
+        Name = "EC2Test"
+    }
 }
